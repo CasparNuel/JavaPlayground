c5677d2363c329cb33649d7a7a239fa890083321
Caspar Nuël <caspar.nuel@student.howest.be>
2020-11-09 22:19:59 +0100

    Added Example2.java

---
+++ src/Example2.java
@@ -0,0 +1,20 @@
+/*
+This demonstrates a variable
+
+This file is called Example2.java
+ */
+
+class Example2 {
+    public static void main(String args[]) {
+        int var1;  // This declares a variable
+        int var2;  // This declares a second variable
+
+        var1 = 1024; // This assigns a value to the variable 'var1'
+
+        System.out.println("var1 contains: " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.println("var2 contains: " + var2);
+    }
+}
