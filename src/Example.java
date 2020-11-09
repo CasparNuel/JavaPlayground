7ac044738bfb93fa141cabe1a0185958e8eba88a
Caspar Nuël <caspar.nuel@student.howest.be>
2020-11-09 21:33:58 +0100

    Created Examples.java and successfully executed the Example class.

---
+++ src/Example.java
@@ -0,0 +1,14 @@
+/*
+This is a simple Java Program
+
+This file is called Example.java
+
+Book: Java - A Beginner's Guide.
+ */
+
+class Example {
+    //A java program begins with a call to main().
+    public static void main (String args[]) {
+        System.out.println("Hello world!");
+    }
+}
