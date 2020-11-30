0e3dd194ef9acc7d4dff86699e0f52bf68ff0b35
Caspar Nuël <caspar.nuel@student.howest.be>
2020-11-30 21:45:47 +0100

    Used shorthand for 'count = count + 1'

--- src/ForDemo.java
+++ src/ForDemo.java
@@ -2,7 +2,7 @@ class ForDemo {
     public static void main (String args[]) {
         int count;
 
-        for(count = 0; count < 5; count = count + 1)
+        for(count = 0; count < 5; count++)
             System.out.println("This is the current count:" + count);
 
         System.out.println("Done!");
