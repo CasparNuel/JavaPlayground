d332eba938168ecdd8358380ea5b6a899b3c4602
Caspar Nuël <caspar.nuel@student.howest.be>
2020-11-30 22:19:22 +0100

    Added assignment MoonGravity

---
+++ src/MoonGravity.java
@@ -0,0 +1,13 @@
+class MoonGravity {
+    public static void main (String args[]) {
+        double myEarthWeight, myMoonWeight;
+
+        myEarthWeight = 55;
+
+        System.out.println("My weight on earth is " + myEarthWeight);
+
+        myMoonWeight = myEarthWeight * 0.17;
+
+        System.out.println("My weight on earth is " + myMoonWeight);
+    }
+}
